d77b8db9e71f1f5fd6d04dcb1ba714b7e9c8662f
Rutger <1201954@student.roc-nijmegen.nl>
2023-05-24 14:23:21 +0200

    shortcut to start menu working

--- fileUtilities/RegisterApp.java
+++ fileUtilities/RegisterApp.java
@@ -1,23 +1,29 @@
 package fileUtilities;
 
 import errorHandeling.Error;
-import mslinks.ShellLink;
+import mslinks.ShellLinkHelper;
 
 import java.nio.file.Paths;
 
 public class RegisterApp {
 
-
+   /**
+    * Creates a shortcut in the start menu
+    */
    public RegisterApp() {
       // get data path
       String absolutePath = Paths.get("").toAbsolutePath().normalize().toString();
-      String dataPath = Paths.get(ClickerData.WINDOWS_DRIVE + "Users/" + ClickerData.USER + "/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/").toAbsolutePath().normalize().toString();
+      String dataPath = Paths.get(ClickerData.WINDOWS_DRIVE + "Users/" + ClickerData.USER + "/AppData/Roaming/Microsoft/Windows/Start Menu/Programs").toAbsolutePath().normalize().toString();
+
+      String autoclicker = "/Useful-Autoclicker.jar";
+      String autoclickerLink = "/Useful-Autoclicker.lnk";
+
+      String targetPath = absolutePath + autoclicker;
+      String linkPath = dataPath + autoclickerLink;
 
       try {
-         Error.showError("Created shortcut here", "Created shortcut here: " + dataPath + "/Useful-Autoclicker.lnk\n Shortcut from here: " + absolutePath + "/Useful-Autoclicker.jar", "Created shortcut here: " + dataPath + "/Test.lnk\n Shortcut from here: " + absolutePath);
-
-         ShellLink.createLink(absolutePath + "/Useful-Autoclicker.jar", dataPath + "/Useful-Autoclicker.lnk");
-
+         ShellLinkHelper.createLink(targetPath, linkPath);
+         Error.showError("Created shortcut here", "Created shortcut here: " + linkPath + "\n Shortcut from here: " + targetPath, "Created shortcut here: " + linkPath + "\n Shortcut from here: " + targetPath);
       } catch (Exception e) {
          Error.showError("Error creating Link", e.toString(), e.toString());
          e.printStackTrace();
