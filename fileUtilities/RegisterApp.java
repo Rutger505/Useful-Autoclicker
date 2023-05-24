6be7b9ee248a9d24724a2dbf2a3da96dcf99b87e
Rutger <1201954@student.roc-nijmegen.nl>
2023-05-24 13:27:51 +0200

    Correct paths not yet working

--- fileUtilities/RegisterApp.java
+++ fileUtilities/RegisterApp.java
@@ -1,9 +1,26 @@
 package fileUtilities;
 
+import errorHandeling.Error;
+import mslinks.ShellLink;
+
+import java.nio.file.Paths;
+
 public class RegisterApp {
+
+
    public RegisterApp() {
       // get data path
-      String dataPath = ClickerData.WINDOWS_DRIVE + "Users/" + ClickerData.USER + "/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/";
+      String absolutePath = Paths.get("").toAbsolutePath().normalize().toString();
+      String dataPath = Paths.get(ClickerData.WINDOWS_DRIVE + "Users/" + ClickerData.USER + "/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/").toAbsolutePath().normalize().toString();
 
+      try {
+         Error.showError("Created shortcut here", "Created shortcut here: " + dataPath + "/Useful-Autoclicker.lnk\n Shortcut from here: " + absolutePath + "/Useful-Autoclicker.jar", "Created shortcut here: " + dataPath + "/Test.lnk\n Shortcut from here: " + absolutePath);
+
+         ShellLink.createLink(absolutePath + "/Useful-Autoclicker.jar", dataPath + "/Useful-Autoclicker.lnk");
+
+      } catch (Exception e) {
+         Error.showError("Error creating Link", e.toString(), e.toString());
+         e.printStackTrace();
+      }
    }
 }
