1482c89794d026481b843b53b959baa482086c65
Rutger <1201954@student.roc-nijmegen.nl>
2023-05-24 11:34:40 +0200

    Made class and got data path to it

---
+++ fileUtilities/RegisterApp.java
@@ -0,0 +1,9 @@
+package fileUtilities;
+
+public class RegisterApp {
+   public RegisterApp() {
+      // get data path
+      String dataPath = ClickerData.WINDOWS_DRIVE + "Users/" + ClickerData.USER + "/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/";
+
+   }
+}
