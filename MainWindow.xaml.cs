2afc5eaa89e90a510f04858a1f4d6c14d43246ed
Rutger <1201954@student.roc-nijmegen.nl>
2023-06-15 21:24:23 +0200

    Made some header elements

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Useful_Autoclicker
 {
-    public class MainWindow
+    public partial class MainWindow : Window
     {
-       
+
+
+        private void Window_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+
     }
 }
